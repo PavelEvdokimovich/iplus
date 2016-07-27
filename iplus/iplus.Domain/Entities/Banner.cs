ee1a5150112965dde84508b9de86c25fc99b0553
Pavel <pavel.evdokimovich@gmail.com>
2016-07-27 15:08:14 +0300

    Data model

---
+++ iplus/iplus.Domain/Entities/Banner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iplus.Domain.Entities
+{
+	public class Banner
+	{
+		public int BannerId { get; set; }
+
+		public string Name { get; set; }
+
+		public string Text { get; set; }
+
+		public byte[] ImageData { get; set; }
+
+		public string ImageMimeType { get; set; }
+	}
+}
